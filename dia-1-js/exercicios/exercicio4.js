ee0121f3608651dc03dc9f8fa3ea562b19e7ada7
Gustavo Richardson <gustavo250999@hotmail.com>
2023-03-28 19:34:04 -0300

    tava todo bagunçado exercicio4 e errado, corrigido pelo gabarito

--- dia-1-js/exercicios/exercicio4.js
+++ dia-1-js/exercicios/exercicio4.js
@@ -1,10 +1,15 @@
-let a = 60
+let a = 20
 let b = 60  
 let c = 60
 
-if (a + b + c === 180){
-  console.log(true);
+let invalido = a > 0 && b > 0 && c > 0
+
+if(invalido){
+  if(a + b + c === 180){
+    console.log(true)
+  } else{
+    console.log(false)
+  }
 } else{
-  console.log(false)
-  console.log("Error")
+  console.log("Angulo Invalido")
 }
