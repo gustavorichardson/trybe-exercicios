9fdd0a6378332601c2354b65237d40285677a53a
Gustavo Richardson <gustavo250999@hotmail.com>
2023-03-28 18:56:28 -0300

    feito 1º e 2º exercicio

---
+++ dia-1-js/exercicios/exercicio1.js
@@ -0,0 +1,11 @@
+let a = 11
+let b = 13
+
+if(a > b){
+  console.log("A é maior que B")
+}else if (b > a) {
+  console.log("B é maior que A")
+}else{
+  console.log("A e B são iguais")
+}
+
---
+++ dia-1-js/exercicios/exercicio2.js
@@ -0,0 +1,13 @@
+let a = 11
+let b = 23
+let c = 44
+
+if(a > b && a > c){
+  console.log("A é maior que B e C")
+}else if (b > a && b > c) {
+  console.log("B é maior que A e C")
+}else if (c > a && c > b){
+  console.log("C é maior que A e B")
+} else{
+  console.log("A B e C são iguais")
+}
