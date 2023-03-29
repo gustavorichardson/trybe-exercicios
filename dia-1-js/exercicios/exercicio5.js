62c164c55462ec14eb8ca8de335c34876ebf4d44
Gustavo Richardson <gustavo250999@hotmail.com>
2023-03-29 15:13:23 -0300

    exercicio5 incompleto ainda, finalizarei

---
+++ dia-1-js/exercicios/exercicio5.js
@@ -0,0 +1,14 @@
+r
+let peao;
+
+switch(peao){
+  case 1:
+    console.log(peao = peao1mov ('andar 2 casas'));
+    break;
+  case 2:
+    console.log(peao = peao2mov ('andar 1 casa'));
+    break;
+  default:
+    console.log("Movimento Invalido")
+
+}
