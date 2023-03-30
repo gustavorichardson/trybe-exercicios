e5513cae46d141744b2b536d91ea245200c9e1dc
Gustavo Richardson <gustavo250999@hotmail.com>
2023-03-30 15:37:46 -0300

    algumas alterações no codigo

--- dia-2-js/exercicios/exercicio1.js
+++
@@ -1,7 +0,0 @@
-//Obtenha o valor “Serviços” do array 
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
-
-console.log(menuServices);
-
--- dia-2-js/exercicios/exercicio2.js
+++
@@ -1,5 +0,0 @@
-//Procure o índice do valor “Portfólio” do array
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf('Portfólio') ;
-
-console.log(indexOfPortfolio);
